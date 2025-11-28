b390dbcae1fe368104e08577e3546bde27821b5b
Kesiah-l <klopes002@ensc.fr>
2025-11-28 13:47:08 +0100

    introduction

---
+++ Assets/Script/menuScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuScript : MonoBehaviour
+{
+    public void triggerMenu(int trigger)
+    {
+        switch (trigger)
+        {
+            case 0:
+                Debug.Log("Chargement scène GameScene");
+                SceneManager.LoadScene("GameScene");
+                break;
+            case 1:
+                Application.Quit();
+                break;
+        }
+    }
+}
